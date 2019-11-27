6db50d06cae959d0cb888e0953b33383ae4893c6
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-27 15:38:34 +0900

    [add]ステップ３実装

--- training.rb
+++ training.rb
@@ -17,21 +17,39 @@
 
 # 作成した自動販売機に入れたお金を返してもらう
 # vm.return_money
+class Buy_sale
+  def buy_juice
+    if @slot_money >= @juice.price && @juice.number >= 1
+      puts 'コーラが出てきた'
+      @juice.number -= 1
+      total_sale
+      return_money
+    elsif @juice.number == 0
+      puts '赤いばってん'
+    end
+  end
 
-class VendingMachine
+  def total_sale
+    @slot_money -= @juice.price
+    @sale += @juice.price
+  end
+end
+
+
+
+class VendingMachine < Buy_sale
   # ステップ０　お金の投入と払い戻しの例コード
   # ステップ１　扱えないお金の例コード
-
+  # include Buy
   # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
   MONEY = [10, 50, 100, 500, 1000].freeze
-  attr_accessor
-
+  attr_accessor :juice, :sale
   # （自動販売機に投入された金額をインスタンス変数の @slot_money に代入する）
   def initialize
     # 最初の自動販売機に入っている金額は0円
     @slot_money = 0
     @juice = Juice.new
-    puts @juice.name
+    @sale = 0
   end
 
   # 投入金額の総計を取得できる。
@@ -72,4 +90,9 @@ class Juice
   # end
 end
 
-VendingMachine.new
+
+
+vend = VendingMachine.new
+vend.slot_money(500)
+vend.buy_juice
+puts vend.sale
