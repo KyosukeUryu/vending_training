4013e9b60d8e574ac4d9b99420272b5fe050cebf
kyosuke uryu <kyo.uryu33@gmail.com>
2020-01-27 15:23:08 +0900

    [add]test完了

--- test/test.rb
+++ test/test.rb
@@ -4,8 +4,33 @@ require './lib/Juice'
 require './lib/User'
 
 class VendingMachineTest < Minitest::Test
-  def test_new_user
-    user = User.new
-    assert_equal user.name, 'taro'
+  def setup
+    @vend = VendingMachine.new
+    @user = User.new
+    @vend.stock_first_juice
+    @user.slot_money(100, @vend)
+    @user.slot_money(10, @vend)
+    @user.slot_money(10, @vend)
+  end
+
+  def test_stock_first_juice
+    assert_equal @vend.stock.count, 3
+    assert_equal @vend.stock.keys.first, 'コーラ'
+    assert_equal @vend.stock.values.first.count, 5
+    assert_equal @vend.juice_price_list.first, 120
+    assert_equal @vend.juice_name_list.first, 'コーラ'
+  end
+
+  def test_slot_money
+    assert_equal @vend.current_slot_money, 120
+    refute @user.slot_money(1, @vend)
+    refute @user.slot_money(10000, @vend)
+  end
+
+  def test_buy_juice
+    @vend.buy_juice(2, @user)
+    assert_equal 4, @vend.stock["水"].count
+    assert_equal 100, @vend.sale
+    assert_equal 5, @vend.stock["コーラ"].count
   end
 end
